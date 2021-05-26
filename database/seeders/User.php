f73cd2b386e5e6445bae3d6b71d714cbd96580d5
Khoby-790 <ebaidoo79@gmail.com>
2021-05-26 21:11:24 +0000

    --- Auto-Git Commit ---

--- database/seeders/User.php
+++ database/seeders/User.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User as ModelsUser;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class User extends Seeder
@@ -13,6 +15,23 @@ class User extends Seeder
      */
     public function run()
     {
-        //
+        $password = bcrypt("secretpassword");
+        ModelsUser::truncate();
+
+        $faker = Factory::create();
+
+        ModelsUser::create([
+            "name" => 'John Doe',
+            "password" => $password,
+            "email" => "ebaidoo79@gmail.com"
+        ]);
+
+        foreach (range(1, 10) as $i) {
+            ModelsUser::create([
+                "name" => $faker->name,
+                "password" => $password,
+                "email" => $faker->email
+            ]);
+        }
     }
 }
